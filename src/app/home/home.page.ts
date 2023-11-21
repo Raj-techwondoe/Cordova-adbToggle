ce102de4b71e9f46618b798ace193f521a502ed6
Raj-techwondoe <raj.upadhyay@techwondoe.com>
2023-11-21 17:20:32 +0530

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+declare let cordova: any;
+declare let rootdetection: any;
+declare let custom: any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  adbStatus: string | undefined;
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
+  initializeApp() {
+    this.platform.ready().then(() => {
+      if (typeof rootdetection !== 'undefined') {
+        this.checkRootStatus();
+      } else {
+        console.log(
+          'Cordova is not available. Make sure you are running on a device or emulator.'
+        );
+      }
+    });
+  }
+
+  checkRootStatus() {
+    rootdetection.isDeviceRooted(
+      (isRooted: any) => {
+        console.log('Is device rooted?' + isRooted);
+        // @ts-ignore
+        window.ShellExec.exec('su', function (res) {
+          console.log('exit status: ' + res.exitStatus);
+          console.log('cmd output: ' + res.output);
+        });
+      },
+      (error: any) => {
+        console.log('Error checking root status' + JSON.stringify(error));
+      }
+    );
+  }
+
+  enable() {
+    custom.enableUSBDebugging(
+      () => this.getStatus(),
+      (error: any) => console.error('Error enabling USB debugging', error)
+    );
+  }
+  disable() {
+    custom.disableUSBDebugging(
+      () => this.getStatus(),
+      (error: any) => console.error('Error enabling USB debugging', error)
+    );
+  }
+  getStatus() {
+    custom.getDebuggingStatus(
+      (value: any) => {
+        this.adbStatus = value;
+      },
+      (error: any) => console.error('Error enabling USB debugging', error)
+    );
+  }
 }
